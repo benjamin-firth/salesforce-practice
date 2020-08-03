329a8a487cf154d7e91776b1411bfcade533d5ea
Ben Firth <bfirth93@gmail.com>
2020-08-03 17:44:34 -0600

    Stage five

--- BenjaminFirth_Slalom_Takehome/force-app/main/default/lwc/contactForm/contactForm.js
+++ BenjaminFirth_Slalom_Takehome/force-app/main/default/lwc/contactForm/contactForm.js
@@ -1,15 +1,36 @@
-import { LightningElement, wire } from 'lwc';
+import { LightningElement, wire, api, track } from 'lwc';
+import { refreshApex } from '@salesforce/apex';
 import getContactList from '@salesforce/apex/ContactController.getContactList';
 
-const columns = [
-  { label: 'Name', fieldName: 'Name' },
-  { label: 'Title', fieldName: 'Title', sortable: true},
-  { label: 'Phone', fieldName: 'Phone', type: 'phone' },
-  { label: 'Email', fieldName: 'Email', type: 'email' },
-];
-
 export default class ContactForm extends LightningElement {
-  @wire(getContactList) contacts;
-  columns = columns;
-  
+  @track error;
+  @track data = [];
+  @api sortedDirection = 'asc';
+  @api sortedBy = 'Name';
+  result;
+  @track columns = [
+    { label: 'Name', fieldName: 'Name' },
+    { label: 'Title', fieldName: 'Title', sortable: true},
+    { label: 'Phone', fieldName: 'Phone', type: 'phone' },
+    { label: 'Email', fieldName: 'Email', type: 'email' },
+  ];
+
+  @wire(getContactList, {sortBy: '$sortedBy', sortDirection: '$sortedDirection' })
+  contactCheck(result) {
+    this.result = result;
+    if (result.data) {
+      this.data = result.data;
+    } else if (result.error) {
+      this.error = result.error;
+    }
+  }
+
+  sortColumns( event ) {
+    this.sortedBy = event.detail.fieldName;
+    this.sortedDirection = event.detail.sortDirection;
+    return refreshApex(this.result);
+  }
+
+  // It seems like this functionality could be done in either JS or Apex. 
+  // Is there a reason you might choose one over the other?
 }
